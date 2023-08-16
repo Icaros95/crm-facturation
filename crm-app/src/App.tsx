5a153c45cb2ca147a9317924f94dd43baf97d87e
Nana Villarroel <icarus851005@gmail.com>
2023-08-16 17:22:02 -0300

    routing of pages and creation of relevant folders

--- crm-app/src/App.tsx
+++ crm-app/src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
+import Dashboard from './pages/1.dashboard/Dashboard';
+import Customers from './pages/2.customers/Customers';
+import Items from './pages/3.Items/Items';
+import Estimates from './pages/4.Estimates/Estimates';
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <h1>Hello CRM</h1>        
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/customers' element={<Customers />} />
+          <Route path='/items' element={<Items />} />
+          <Route path='/estimates' element={<Estimates />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ crm-app/src/pages/1.dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import '../../App.css';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis ut aperiam ex quisquam expedita labore doloribus dolore. Voluptas esse asperiores adipisci delectus perspiciatis quidem modi distinctio, rerum beatae, fuga blanditiis!</p>
+        </div>
+    )
+}
+
+export default Dashboard;
