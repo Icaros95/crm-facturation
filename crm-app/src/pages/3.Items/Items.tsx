6c90cdc61dba706b0d17b7e43177025b070aaa76
Belen Delgadillo <belendelgadilloarias@gmail.com>
2023-08-19 00:43:38 -0300

    agrega header

--- crm-app/src/pages/3.Items/Items.tsx
+++ crm-app/src/pages/3.Items/Items.tsx
@@ -1,8 +1,11 @@
 import '../../App.css';
+import Header from "../../components/Header/Header";
+
 
 const Items = () => {
     return (
         <div>
+            <Header /> 
             <h1>Items</h1>
             <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis ut aperiam ex quisquam expedita labore doloribus dolore. Voluptas esse asperiores adipisci delectus perspiciatis quidem modi distinctio, rerum beatae, fuga blanditiis!</p>
         </div>
